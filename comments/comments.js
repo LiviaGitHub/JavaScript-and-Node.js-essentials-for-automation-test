88d30ea1d690641a3a3d7818fa191b598797112e
LiviaGitHub <liviagithub@gmail.com>
2022-12-22 10:48:34 +0100

    adding more tips

--- comments/comments.js
+++ comments/comments.js
@@ -10,3 +10,6 @@
 function sum(num1, num2){
     return num1 + num2
 }
+
+// type 'clg' this is the shortcut to create console.log
+// console.log('test shortcut');
--- variables/variables.js
+++ variables/variables.js
@@ -1,16 +1,47 @@
 /* 
-* var, const, let
-* variables always start with a letter, dollar ($), or underscore (_). 
-* variables are case sensitive.
-* variables needs to be unique.
-* variables cannot be named using the system keyboard. Example: let throw = "print"
-* possible declare variables with none value. Example: let green
+*  var, const, let
+*  variables always start with a letter, dollar ($), or underscore (_). 
+*  variables are case sensitive.
+*  variables needs to be unique.
+*  variables cannot be named using the system keyboard. Example: let throw = "print"
+*  use [let] or [const] over [var] because they support block scoping.
 */
 
-// type 'clg' this is the shortcut to create console.log
-console.log(green);
+/* 
+*  Rules for [const] variable
+*  [const] variable must be initialized with a value.
+*  [const] variable cannot be re-assigned.
+*/
 
-let green = "print green"
+// Example: variable must be initialized with a value.
+const initializedWithValue = "VALUE"
+console.log(initializedWithValue + 'this will WORK');
 
-// type 'clg' this is the shortcut to create console.log
-console.log(green);
+const initializedWithNoneValue
+console.log(initializedWithNoneValue + 'this will NOT WORK');
+
+// Example: variable cannot be re-assigned. 
+const cannotBeReassigned = "green"
+cannotBeReassigned = "pink"
+console.log(cannotBeReassigned + 'this will NOT WORK');
+
+/* 
+*  Rules for [let] variable
+*  [let] possible declare variables with none value.
+*  [let] variable can be re-assigned.
+*  [let] variable can be initialized with a number and re-assigned with a string.
+*/
+
+// Example: variable can be re-assigned. 
+let letRules = "green"
+letRules = "pink"
+console.log(letRules + 'this will print [pink] the last value');
+
+// Example: javascript variable can be initialized with a number and re-assigned with a string.
+let number = 10
+number = "string"
+console.log(letRules + 'this will print [string] the last value');
+
+// Example: possible declare variables with none value.
+let noneValues
+console.log(noneValues);
